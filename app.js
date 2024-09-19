5afeb83e0d1169f939a985b4da0d2410622ff894
CARLRAM2482 <145763670+CARLRAM2482@users.noreply.github.com>
2024-09-18 21:52:17 -0500

    v1.1

--- app.js
+++ app.js
@@ -1,1 +1,45 @@
 const apiurl = "https://rickandmortyapi.com/api/character"
+
+function makecard (character){
+    const {name, status, image} = character;
+    const cardsContainer = document.querySelector(".cards-container");
+
+    const title = document.createElement("h5");
+    title.textContent = name;
+
+    const characterstatus = document.createElement("p");
+    characterstatus.textContent = status;
+    if(status == "Alive") characterstatus.style.color = "blue";
+    else characterstatus.style.color = "gray";
+    const characterimge = document.createElement("img");
+    characterimge.src = image;
+    characterimge.width = 200;
+
+    const card = document.createElement("div");
+    card.appendChild(title);
+    card.appendChild(characterimge);
+    card.appendChild(characterstatus);
+    card.style.backgroundColor = "red";
+
+    cardsContainer.appendChild(card);
+
+
+
+}
+async  function getCharacters () {
+    try {
+        const response = await  fetch(apiurl);
+        const {results} = await response.json();
+        
+        for(let i= 0; i< results.length;  i++) {
+            makecard(results[1]);
+
+        }
+        console.log(results);
+    } catch (error) {
+        console.error( error);
+    }
+    
+
+}
+getCharacters();
